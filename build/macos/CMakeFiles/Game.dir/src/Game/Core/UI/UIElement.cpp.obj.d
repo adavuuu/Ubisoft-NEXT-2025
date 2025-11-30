043901864b304dcb0b8a4670a4870431d6a2407f
Ada Vu <vu.hanh.ada@gmail.com>
2025-11-29 22:50:53 -0500

    Remove build file and update gitignore to ignore build files

--- build/macos/CMakeFiles/Game.dir/src/Game/Core/UI/UIElement.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Game.dir/src/Game/Core/UI/UIElement.cpp.obj: \
-  /Users/adavu/Desktop/UbisoftNEXT/NEXT-2025-Programming-API/src/Game/Core/UI/UIElement.cpp \
-  /Users/adavu/Desktop/UbisoftNEXT/NEXT-2025-Programming-API/src/Game/Core/UI/UIElement.h
